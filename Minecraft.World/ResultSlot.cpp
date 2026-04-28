41275f0e362f5b816dc9092c134f73739f992abf
SevenToaster509 <andimmcc@gmail.com>
2026-04-28 16:19:35 +0100

    fix: make hoe/sword achievement

--- Minecraft.World/ResultSlot.cpp
+++ Minecraft.World/ResultSlot.cpp
@@ -38,16 +38,17 @@ void ResultSlot::checkTakeAchievements(shared_ptr<ItemInstance> carried)
 	carried->onCraftedBy(player->level, dynamic_pointer_cast<Player>( player->shared_from_this() ), removeCount);
 	removeCount = 0;
 
-	if (carried->id == Tile::workBench_Id)				player->awardStat(GenericStats::buildWorkbench(),		GenericStats::param_buildWorkbench());
-	else if (carried->id == Item::pickAxe_wood_Id)		player->awardStat(GenericStats::buildPickaxe(),			GenericStats::param_buildPickaxe());
-	else if (carried->id == Tile::furnace_Id)			player->awardStat(GenericStats::buildFurnace(),			GenericStats::param_buildFurnace());
-	else if (carried->id == Item::hoe_wood_Id)			player->awardStat(GenericStats::buildHoe(),				GenericStats::param_buildHoe());
-	else if (carried->id == Item::bread_Id)				player->awardStat(GenericStats::makeBread(),			GenericStats::param_makeBread());
-	else if (carried->id == Item::cake_Id)				player->awardStat(GenericStats::bakeCake(),				GenericStats::param_bakeCake());
-	else if (carried->id == Item::pickAxe_stone_Id)		player->awardStat(GenericStats::buildBetterPickaxe(),	GenericStats::param_buildBetterPickaxe());
-	else if (carried->id == Item::sword_wood_Id)		player->awardStat(GenericStats::buildSword(),			GenericStats::param_buildSword());
-	//else if (carried->id == Tile::enchantTable_Id)	player->awardStat(GenericStats::enchantments(),			GenericStats::param_achievement(eAward_));
-	else if (carried->id == Tile::bookshelf_Id)			player->awardStat(GenericStats::bookcase(),				GenericStats::param_bookcase());
+	if (carried->id == Tile::workBench_Id)												player->awardStat(GenericStats::buildWorkbench(),		GenericStats::param_buildWorkbench());
+	else if (carried->id == Item::pickAxe_wood_Id)										player->awardStat(GenericStats::buildPickaxe(),			GenericStats::param_buildPickaxe());
+	else if (carried->id == Tile::furnace_Id)											player->awardStat(GenericStats::buildFurnace(),			GenericStats::param_buildFurnace());
+	else if (carried->getItem()->getBaseItemType() == Item::eBaseItemType_hoe)			player->awardStat(GenericStats::buildHoe(),				GenericStats::param_buildHoe());
+	else if (carried->id == Item::bread_Id)												player->awardStat(GenericStats::makeBread(),			GenericStats::param_makeBread());
+	else if (carried->id == Item::cake_Id)												player->awardStat(GenericStats::bakeCake(),				GenericStats::param_bakeCake());
+	else if (carried->id == Item::pickAxe_stone_Id)										player->awardStat(GenericStats::buildBetterPickaxe(),	GenericStats::param_buildBetterPickaxe());
+	// The description is technically wrong but that's accurate
+	else if (carried->getItem()->getBaseItemType() == Item::eBaseItemType_sword)		player->awardStat(GenericStats::buildSword(),			GenericStats::param_buildSword());
+	//else if (carried->id == Tile::enchantTable_Id)									player->awardStat(GenericStats::enchantments(),			GenericStats::param_achievement(eAward_));
+	else if (carried->id == Tile::bookshelf_Id)											player->awardStat(GenericStats::bookcase(),				GenericStats::param_bookcase());
 
 	// 4J : WESTY : Added new acheivements. 
 	else if (carried->id == Tile::dispenser_Id) player->awardStat(GenericStats::dispenseWithThis(), GenericStats::param_dispenseWithThis());
